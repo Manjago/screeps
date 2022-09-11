b47cf8e8bba65eff5cfe61eddb44af3c30136aa2
Kirill Temnenkov <kirill@temnenkov.com>
2022-09-11 10:12:01 +0300

    refactoring - logic.spawn

---
+++ logic.spawn.js
@@ -0,0 +1,40 @@
+var logicSpawn = {
+
+    run: function () {
+
+
+        if (Game.spawns['Spawn1'].spawning) {
+            return;
+        }
+
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+
+        if (harvesters.length < 2) {
+            var newName = 'Harvester' + Game.time;
+            console.log('Spawning new harvester: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
+                { memory: { role: 'harvester' } });
+        }
+
+        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+
+        if (upgraders.length < 1) {
+            var newName = 'Upgrader' + Game.time;
+            console.log('Spawning new upgrader: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
+                { memory: { role: 'upgrader' } });
+        }
+
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+        if (builders.length < 2) {
+            var newName = 'Builder' + Game.time;
+            console.log('Spawning new builder: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
+                { memory: { role: 'builder' } });
+        }
+
+    }
+};
+
+module.exports = logicSpawn;
